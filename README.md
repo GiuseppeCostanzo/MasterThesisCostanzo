a7b4af6829699a60a86900694aecf059f97fda9a
GiuseppeCostanzo <64311248+GiuseppeCostanzo@users.noreply.github.com>
2023-09-21 19:07:30 +0200

    Mapping movements (draft)

--- README.md
+++
@@ -1,2 +0,0 @@
-# Hand_code_thesis
- 
--- base_arduino/base_arduino.ino
+++ base_arduino/base_arduino.ino
@@ -1,12 +1,19 @@
 #include <Servo.h>
-Servo myservo;
+
+// Definizione dei servomotori
+Servo thumb;
+Servo index_finger;
+Servo middle_finger;
+Servo ring_finger;
+Servo pinky;
+Servo forearm;
+
 String inByte;
 int pos;
 
 void setup() {
-
-  myservo.attach(9);
   Serial.begin(9600);
+  index_finger.attach(9);
 }
 
 void loop() {
@@ -14,8 +21,8 @@ void loop() {
   {
     inByte = Serial.readStringUntil('\n');  // read data until newline
     pos = inByte.toInt();                   // change datatype from string to integer
-    myservo.write(pos);                     // move servo
-    Serial.print("Servo in position: ");
-    Serial.println(inByte);
+    index_finger.write(pos);                     // move servo
+    //Serial.print("Servo in position: ");
+    //Serial.println(inByte);
   }
 }
