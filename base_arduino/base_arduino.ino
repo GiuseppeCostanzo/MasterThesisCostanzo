5b5384cefdf71dda1fdd9f6fa4b7590c9eaa3661
GiuseppeCostanzo <64311248+GiuseppeCostanzo@users.noreply.github.com>
2023-10-05 16:29:07 +0200

    Some improvements

--- base_arduino/base_arduino.ino
+++ base_arduino/base_arduino.ino
@@ -8,6 +8,7 @@ Servo ring_finger;
 Servo pinky;
 Servo forearm;
 
+int i = 0;
 //String inByte;
 //int pos;
 int receivedData[6];
@@ -24,28 +25,44 @@ void setup() {
   forearm.attach(11);
 
   //initial position of each servo
-  thumb.write(90);
-  index_finger.write(90);
-  middle_finger.write(90);
-  ring_finger.write(90);
-  pinky.write(90);
-  forearm.write(90); 
+  //thumb.write(35);
+  //index_finger.write(90);
+  //middle_finger.write(90);
+  //ring_finger.write(90);
+  //pinky.write(90);
+  //forearm.write(90); 
+  receivedData[0] = 0;
+  receivedData[1] = 0;
+  receivedData[2] = 0;
+  receivedData[3] = 0;
+  receivedData[4] = 0;
+  receivedData[5] = 0;
+
 }
 
 void loop() {
-  // if data available in serial port
-  if (Serial.available()){
-    for (int i = 0; i < 6; i++) {
-      receivedData[i] = Serial.read();
+  if (Serial.available() > 0) {
+    receivedData[i] = Serial.read();
+    Serial.print(receivedData[i], DEC);
+    Serial.print("&");
+    i = i+1;
+
+
+  if(i >= 6){
+    Serial.print("Letti 6 valori$");
+    thumb.write(receivedData[0]);
+    index_finger.write(receivedData[1]);
+    middle_finger.write(receivedData[2]);
+    ring_finger.write(receivedData[3]);
+    pinky.write(receivedData[4]);
+    forearm.write(receivedData[5]); 
     }
   }
-
-  thumb.write(receivedData[0]);
-  index_finger.write(receivedData[1]);
-  middle_finger.write(receivedData[2]);
-  ring_finger.write(receivedData[3]);
-  pinky.write(receivedData[4]);
-  forearm.write(receivedData[5]); 
-  delay(50);
+  // if data available in serial port
+  /*if (Serial.available() > 0){
+    Serial.print("Qualcosa available$ ");
+    for (int i = 0; i < 6; i++) {
+      receivedData[i] = Serial.read();
+    }*/
 
 }
