0c542b9f7fbdabc245777c1673d00e49016ed938
GiuseppeCostanzo <64311248+GiuseppeCostanzo@users.noreply.github.com>
2024-03-20 18:58:45 +0100

    Improvements arduino code

--- base_arduino/base_arduino.ino
+++ base_arduino/base_arduino.ino
@@ -1,60 +1,46 @@
 #include <Servo.h>
 
-// Servo definitions
+int x; 
 Servo thumb_big;
 Servo thumb_little;
 Servo index_finger;
 Servo middle_finger;
 Servo ring_pinky;
 Servo forearm;
-
-int i = 0;
-//String inByte;
-//int pos;
 int receivedData[6];
+int i = 0;
 
-void setup() {
-  Serial.begin(9600);
 
-  // communication pin for each servo
+void setup() { 
+	Serial.begin(115200); 
+	Serial.setTimeout(1); 
   thumb_big.attach(9);
   thumb_little.attach(11);
   index_finger.attach(6);
   middle_finger.attach(10);
   ring_pinky.attach(5);
   forearm.attach(3);
-
-  receivedData[0] = 0;
-  receivedData[1] = 0;
-  receivedData[2] = 0;
-  receivedData[3] = 0;
-  receivedData[4] = 0;
-  receivedData[5] = 0;
-
-}
-
-void loop() {
-  if (Serial.available() > 0) {
-    receivedData[i] = Serial.read();
-    Serial.print(receivedData[i], DEC);
-    Serial.print("&");
+} 
+void loop() { 
+	while (Serial.available()){
     i = i+1;
-    }
-
-    // Controlla i servo motori con i valori ricevuti
-    thumb_big.write(receivedData[0]);
-    thumb_little.write(receivedData[1]);
-    index_finger.write(receivedData[2]);
-    middle_finger.write(receivedData[3]);
-    ring_pinky.write(receivedData[4]);
-    forearm.write(receivedData[5]);
+    delay(10);
+    Serial.print(i);
+    receivedData[i] = Serial.readString().toInt(); 
+  } 
   
-  // if data available in serial port
-  /*if (Serial.available() > 0){
-    Serial.print("Qualcosa available$ ");
-    for (int i = 0; i < 6; i++) {
-      receivedData[i] = Serial.read();
-    }
-  }*/
-
-}
+  if (i>=6){
+    i = 0;
+    thumb_big.write(receivedData[1]);
+    delay(10);
+    thumb_little.write(receivedData[2]);
+    delay(10);
+    index_finger.write(receivedData[3]);
+    delay(10);
+    middle_finger.write(receivedData[4]);
+    delay(10);
+    ring_pinky.write(receivedData[5]);
+    delay(10);
+    forearm.write(receivedData[6]);
+  }
+} 
