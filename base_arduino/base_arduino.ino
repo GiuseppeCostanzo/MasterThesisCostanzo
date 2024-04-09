b045b52b69ade2b6434b7ef99f9a5e008c61d3a4
GiuseppeCostanzo <64311248+GiuseppeCostanzo@users.noreply.github.com>
2024-04-09 22:35:36 +0200

    Update base_arduino.ino

--- base_arduino/base_arduino.ino
+++ base_arduino/base_arduino.ino
@@ -9,7 +9,7 @@ Servo ring_pinky;
 Servo forearm;
 int receivedData[6];
 int i = 0;
-
+bool done = false;
 
 void setup() { 
 	Serial.begin(38400); 
@@ -27,23 +27,47 @@ void setup() {
   ring_pinky.write(0);
   forearm.write(0);
 
+  pinMode(13, OUTPUT);
+
   for(int a = 0; a<=5; a++){
     receivedData[a] = 1;
   }
+
 } 
 void loop() { 
-	if (Serial.available()){
-    receivedData[i] = Serial.read(); 
-    i = i+1;
-  } 
-  
-  if (i>=6){
-    i = 0;
-    thumb_big.write(receivedData[0]);
-    thumb_little.write(receivedData[1]);
-    index_finger.write(receivedData[2]);
-    middle_finger.write(receivedData[3]);
-    ring_pinky.write(receivedData[4]);
-    forearm.write(receivedData[5]);
+	if (!Serial.available()){
+    digitalWrite(13, LOW);
+    if(done==false){
+      thumb_big.write(receivedData[0]);
+      thumb_little.write(receivedData[1]);
+      index_finger.write(receivedData[2]);
+      middle_finger.write(receivedData[3]);
+      ring_pinky.write(receivedData[4]);
+      forearm.write(receivedData[5]);
+      // For debug
+      /*Serial.print(receivedData[0]);
+      Serial.print("-");
+      Serial.print(receivedData[1]);
+      Serial.print("-");
+      Serial.print(receivedData[2]);
+      Serial.print("-");
+      Serial.print(receivedData[3]);
+      Serial.print("-");
+      Serial.print(receivedData[4]);
+      Serial.print("-");
+      Serial.print(receivedData[5]);
+      Serial.println();*/
+      done = true;
+    }
   }
+
+  if(Serial.available()){
+    digitalWrite(13, HIGH);
+    done = false;
+    for(int a=0; a<6; a++){
+      receivedData[a] = Serial.read(); 
+      delay(10); //necessario
+    }
+  } 
+
 } 
