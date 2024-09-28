393c6d60d67e8bebcd89f1d03191cea6640b1e2e
GiuseppeCostanzo <64311248+GiuseppeCostanzo@users.noreply.github.com>
2024-09-28 17:39:04 +0200

    Modified sinusoidal discretize

--- Envv/Discretizer.py
+++ Envv/Discretizer.py
@@ -2,150 +2,11 @@ import numpy as np
 from abc import ABC, abstractmethod
 from Utility import Toolbox
 
-
-class PreDiscr():
-
-    # Prepare the data for discretization, and then call the corresponding discretize
-    # Parameter in input "item" is the selected item (dict)
-    def pre_discretize(item, deltaT=None):
-        if item is None:
-            return None
-
-        if len(item) == 0:  # Se la lunghezza del dizionario/lista  è 0, è vuoto oppure è None
-            return None
-        
-        #if the item instance is a dictionary, implies that is a simple movement
-        # otherwise is a list of dictionary and so a complex movement
-        if isinstance(item, dict):
-            if item["type"] == "linear":
-                values = item["values"] #values(movement) of the item 
-                
-                start_time = int(values[0][6])
-                end_time = int(values[1][6])
-
-                start_pos = []
-                for i in range(0,len(values[0])-1):
-                    start_pos.append(int(values[0][i]))
-
-                end_pos = []
-                for i in range(0,len(values[1])-1):
-                    end_pos.append(int(values[1][i]))
-                
-                # for complex movement - smaller deltaT passed
-                if deltaT is None:
-                    deltaT = int(values[2]) 
-
-                linMov = LinearMovement(start_time,end_time,start_pos,end_pos,deltaT)
-                #movement to visualize/execute
-                movement = (linMov.discretize()).tolist()
-                
-                # Check if e > 100 (escludendo l'ultima colonna) - exist_cut_values: bool
-                exists_cut_values = False
-                exists_cut_values = any((val > 100 or val < 0) for row in movement for val in row[:-1])
-
-                if exists_cut_values:
-                    for subrow in movement:
-                        for i in range(len(subrow) - 1):  # - 1 for exclude the last column
-                            if subrow[i] > 100:
-                                subrow[i] = 100
-                            elif subrow[i] < 0:
-                                subrow[i] = 0
-                return movement, exists_cut_values
-                
-            if item["type"] == "sinusoidal":
-                values = item["values"] #values(movement) of the item   
-                start_time = int(values[0])
-                end_time = int(values[1])
-                if deltaT is None:
-                    deltaT = int(values[8])
-                
-                amplitude = []
-                for i in range(2,8):
-                    amplitude.append(int(values[i][0]))
-                    
-                frequency = []
-                for i in range(2,8):
-                    frequency.append(int(values[i][1]))
-                
-                phase = []
-                for i in range(2,8):
-                    phase.append(float(values[i][2]))
-                
-                start_value_y = []
-                for i in range(2,8):
-                    start_value_y.append(int(values[i][3]))
-                
-                sinMov = SinusoidalMovement(start_time,end_time,amplitude,frequency,phase,start_value_y,deltaT)
-                movement = (sinMov.discretize()).tolist()
-
-                # Check if e > 100 (escludendo l'ultima colonna)
-                exists_cut_values = False
-                exists_cut_values = any((val > 100 or val < 0) for row in movement for val in row[:-1])
-
-                if exists_cut_values:
-                    for subrow in movement:
-                        for i in range(len(subrow) - 1):  # - 1 for exclude the last column
-                            if subrow[i] > 100:
-                                subrow[i] = 100
-                            elif subrow[i] < 0:
-                                subrow[i] = 0
-                return movement, exists_cut_values
-
-        elif isinstance(item, list):
-            # SAMPLING
-
-            # Sort the dictionary w.r.t to index(level) 
-            r = Toolbox.sort_and_structure(item)
-
-            # Delete the "head" of a complex movements (does not contain values)
-            result_without_head_complex = [diz for diz in r if diz.get("type") != "complex"]
-
-            
-            # Smallest deltaT, t_start and t_end calculation
-            deltaT = 999999
-            t_start = 999999
-            t_end = -1
-            for movement in result_without_head_complex:
-                var = int(movement['values'][-1])
-                if var <= deltaT:
-                    deltaT = var
-                
-                #smallest t_start 
-                if movement['type'] == 'sinusoidal':
-                    var = int(movement['values'][0])
-                    if var <= t_start:
-                        t_start = var
-
-                if movement['type'] == 'linear':
-                    var = int(movement['values'][0][-1])
-                    if var <= t_start:
-                        t_start = var         
-
-                #biggest t_end
-                if movement['type'] == 'sinusoidal':
-                    var = int(movement['values'][1])
-                    if var >= t_end:
-                        t_end = var
-
-                if movement['type'] == 'linear':
-                    var = int(movement['values'][1][-1])
-                    if var >= t_end:
-                        t_end = var  
-
-            complexMov = ComplexMovement(t_start, t_end, deltaT, result_without_head_complex)
-            movement = complexMov.discretize()
-            return movement
-
-        else:
-            print("Error in the pre-discretizer")
-            return None              
-
-
 # Abstract father class
 class Movement(ABC):
     def __init__(self, startTime, endTime):
-        self.startTime = startTime
-        self.endTime = endTime
+        self.startTimePassed = startTime
+        self.endTimePassed = endTime
         #L=(np.round((self.endTime-self.startTime)/deltaT)+1).astype(np.int64)
         #self.DiscreteMov=None
 
@@ -164,16 +25,10 @@ class LinearMovement(Movement):
         
         self.item = item
         self.values = item["values"] # values of the item (movement)
-
-        self.startTimePassed = startTime
-        self.endTimePassed = endTime
+        self.flag = False
         self.deltaT = deltaT
-
         self.startTime = int(self.values[0][6]) #take the start time from the item (json)
         self.endTime = int(self.values[1][6])  #take the end time from the item (json)
-
-        self.flag = False
-
         self.fromPos = [] #take all start pos
         for i in range(0,len(self.values[0])-1):
             if self.values[0][i] == 'NaN':
@@ -188,7 +43,6 @@ class LinearMovement(Movement):
             else:
                 self.toPos.append(int(self.values[1][i]))
 
-
     def discretize(self):
         # Some check
         if all(var is None for var in [self.startTimePassed, self.endTimePassed, self.deltaT]):      
@@ -202,15 +56,14 @@ class LinearMovement(Movement):
         # Discretize
         # Calcola il numero di intervalli discreti
         L = int(np.round((self.endTime - self.startTime) / self.deltaT)) + 1
+        # Genera array di tempi discreti per il set corrente di parametri
+        discrete_times = (np.linspace(self.startTime, self.endTime, L)).astype(np.int64)
 
         L_external = None
         discrete_times_external = None
         if self.flag is True:
             L_external = int(np.round((self.endTimePassed - self.startTimePassed) / self.deltaT)) + 1
             discrete_times_external = (np.linspace(self.startTimePassed, self.endTimePassed, L_external)).astype(np.int64)
-
-        # Genera array di tempi discreti per il set corrente di parametri
-        discrete_times = (np.linspace(self.startTime, self.endTime, L)).astype(np.int64)
 
         discrete_positions = None
         # Restituisce numeri equidistanti in un intervallo specificato L 
@@ -254,14 +107,13 @@ class LinearMovement(Movement):
             result = result.astype(np.int64)
             return result, exists_cut_values
         else:
-            num_rows_large = len(discrete_times_external)
+            num_rows = len(discrete_times_external)
             num_cols = 7
-            large_matrix = np.full((num_rows_large, num_cols), np.nan)
+            large_matrix = np.full((num_rows, num_cols), np.nan)
 
             start_time = result[0, -1]  # ritorna il primo istante di tempo nella matrice piccola
             start_index = int(start_time / self.deltaT)
             large_matrix[start_index:start_index + result.shape[0], :] = result
-
             large_matrix[:, -1] = discrete_times_external
             return large_matrix, exists_cut_values
 
@@ -275,12 +127,15 @@ class SinusoidalMovement(Movement):
     def __init__(self, startTime=None, endTime=None, deltaT=None, item=None):
         super().__init__(startTime, endTime)
 
-        self.flag = False
         if item is None:
             raise TypeError("The 'item' parameter cannot be None")
         
         self.item = item
         self.values = item["values"] # values of the item (movement)  
+        self.deltaT = deltaT
+        self.flag = False
+        self.startTime = int(self.values[0]) #take the start time from the item
+        self.endTime = int(self.values[1]) #...
         
         self.amplitude = []
         for i in range(2,8):
@@ -312,14 +167,11 @@ class SinusoidalMovement(Movement):
             else:
                 self.y_init.append(int(self.values[i][3]))
 
-        self.deltaT = deltaT
 
     def discretize(self):
-        if all(var is None for var in [self.startTime, self.endTime, self.deltaT]):      
-            self.startTime = int(self.values[0]) #take the start time from the item
-            self.endTime = int(self.values[1]) #...
-            self.deltaT = (int(self.values[8]))/1000 #in secondi
-        elif all(var is not None for var in [self.startTime, self.endTime, self.deltaT]):
+        if all(var is None for var in [self.startTimePassed, self.endTimePassed, self.deltaT]):      
+            self.deltaT = (int(self.values[8]))/1000 #take the deltaT from item (json) - to second
+        elif all(var is not None for var in [self.startTimePassed, self.endTimePassed, self.deltaT]):
             self.deltaT = self.deltaT/1000     
             self.flag = True      
         else:
@@ -332,6 +184,15 @@ class SinusoidalMovement(Movement):
         # Genera un array di tempi da startTime a end_time con intervalli di deltaT(periodo di campionamento)
         t = np.arange(self.startTime, self.endTime, self.deltaT)
         t_millis = t*1000 #per l'output si usano i millisecondi
+
+        t_external = None
+        t_millis_external = None
+        if self.flag is True:
+            self.startTimePassed = self.startTimePassed / 1000
+            self.endTimePassed = self.endTimePassed / 1000
+
+            t_external = np.arange(self.startTimePassed, self.endTimePassed, self.deltaT)
+            t_millis_external = t_external*1000 #per l'output si usano i millisecondi
 
         #first element
         column1 = self.amplitude[5] * np.sin(2 * np.pi * self.frequency[5] * t + (self.phase[5]*np.pi)) + self.y_init[5]
@@ -358,8 +219,20 @@ class SinusoidalMovement(Movement):
             # contesto di una movimento complesso più esterno)
             result = np.nan_to_num(result, nan=50)
             result = result.astype(np.int64)
+            return result, exists_cut_values
+        else:
+            num_rows = len(t_millis_external)
+            num_cols = 7
+            large_matrix = np.full((num_rows, num_cols), np.nan)
 
-        return result, exists_cut_values
+            start_time = result[0, -1]  # ritorna il primo istante di tempo nella matrice piccola
+            start_index = int(start_time / (self.deltaT*1000)) #indice di partenza per posizionare la matrice piccola
+            large_matrix[start_index:start_index + result.shape[0], :] = result
+            large_matrix[:, -1] = t_millis_external #metto gli istanti di tempo esterni
+            return large_matrix, exists_cut_values
+
+
+
     
 # item must be a list of dictionary (elements_in_tree_view)
 class ComplexMovement(Movement):
